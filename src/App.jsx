9d0281388c34b68eb682261d8d4e2f1f45a29fc3
khaledazam <khaled141ma@gmail.com>
2025-11-24 18:29:13 +0200

    dock icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '#components/Navbar'
 import Welcome from "#components/Welcome.jsx";
+import {Docks} from "#components/Docks.jsx";
 
 const App = () => {
   return (
     <main>
         <Navbar />
         <Welcome />
+        <Docks />
     </main>
   )
 }
